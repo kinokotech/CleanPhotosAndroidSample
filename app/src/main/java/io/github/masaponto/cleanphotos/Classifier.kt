f8ca2dfb84c0373fac74136928084201fd58d846
masaponto <masaponto@gmail.com>
2018-06-24 17:43:20 +0900

    [Wip]

--- app/src/main/java/io/github/masaponto/cleanphotos/Classifier.kt
+++ app/src/main/java/io/github/masaponto/cleanphotos/Classifier.kt
@@ -1,7 +1,5 @@
 package io.github.masaponto.cleanphotos
 
-import java.nio.channels.FileChannel.MapMode.READ_ONLY
-import android.content.res.AssetFileDescriptor
 import android.app.Activity
 import android.util.Log
 import org.opencv.core.Mat
@@ -13,20 +11,23 @@ import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-class Classifier(val activity: Activity) {
+class Classifier(activity: Activity) {
 
-    val IMAGE_SIZE = 28
-    private lateinit var tffile: Interpreter
-    private var labelProbArray = Array(1, {FloatArray(1)})
-    private val modelName = "cnn_bler.tflite"
+    private val IMAGE_SIZE = 28
+    private val MODEL_NAME = "cnn_bler.tflite"
+    private var tffile: Interpreter
+    private var labelProbArray: Array<FloatArray>
+    private var imageData: ByteBuffer
 
     init {
         tffile = Interpreter(loadModelFile(activity))
+        labelProbArray = Array(1, {FloatArray(1)})
+        imageData = ByteBuffer.allocateDirect(IMAGE_SIZE * IMAGE_SIZE * 3 * 4)
     }
 
     @Throws(IOException::class)
     private fun loadModelFile(activity: Activity): MappedByteBuffer {
-        val fileDescriptor = activity.assets.openFd(modelName)
+        val fileDescriptor = activity.assets.openFd(MODEL_NAME)
         val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
         val fileChannel = inputStream.channel
         val startOffset = fileDescriptor.startOffset
@@ -34,8 +35,7 @@ class Classifier(val activity: Activity) {
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
     }
 
-    private fun convertMattoTfliteInput(matImage: Mat) : ByteBuffer {
-        var imageData = ByteBuffer.allocateDirect(IMAGE_SIZE * IMAGE_SIZE * 3 * 4)
+    private fun convertMatToTfliteInput(matImage: Mat) : ByteBuffer {
         imageData.order(ByteOrder.LITTLE_ENDIAN)
         for (i in 0 until IMAGE_SIZE) {
             for (j in 0 until IMAGE_SIZE) {
@@ -46,9 +46,8 @@ class Classifier(val activity: Activity) {
     }
 
     fun classifyImage(matImage: Mat): Array<FloatArray> {
-        val imageData = convertMattoTfliteInput(matImage)
+        val imageData = convertMatToTfliteInput(matImage)
         tffile.run(imageData, labelProbArray)
-
         return labelProbArray
     }
 
--- app/src/main/java/io/github/masaponto/cleanphotos/MainActivity.kt
+++ app/src/main/java/io/github/masaponto/cleanphotos/MainActivity.kt
@@ -2,11 +2,9 @@ package io.github.masaponto.cleanphotos
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.os.ParcelFileDescriptor
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
@@ -38,51 +36,45 @@ class MainActivity : AppCompatActivity() {
                 val file = File(dir.absolutePath + "/IMG_20180425_122602.jpg")
                 //val file = File(dir.absolutePath + "/test.jpg")
                 if (file.exists()) {
-                    Toast.makeText(this, "File found",Toast.LENGTH_LONG).show()
 
-                    var option = BitmapFactory.Options()
+                    val option = BitmapFactory.Options()
                     option.inSampleSize = 2
-                    option.outHeight = 28
-                    option.outWidth = 28
+
+                    //option.outHeight = 28
+                    //option.outWidth = 28
                     //option.inJustDecodeBounds = false
                     //BitmapFactory.decodeFile(file.path, option)
-
                     //option.inJustDecodeBounds = true
                     //option.inSampleSize = 2
 
                     var bm = BitmapFactory.decodeFile(file.path, option)
                     bm = Bitmap.createScaledBitmap(bm, 28, 28, false)
 
-                    Log.d("path", file.path)
+                    bm?.let {
 
-                    if (bm != null) {
-                        var matImage = Mat(28,28, CvType.CV_32F)
-                        Utils.bitmapToMat(bm, matImage)
+                        // set image view
+                        val imageView: ImageView = findViewById(R.id.imageView)
+                        imageView.setImageBitmap(bm)
 
-                        //matImage.reshape(28,28)
+                        // convert bitmap to Mat
+                        val matImage = Mat(28,28, CvType.CV_32F)
+                        Utils.bitmapToMat(it, matImage)
 
+                        // classification with TF Lite
                         val label = classifier.classifyImage(matImage)
 
-                        Log.d("Result", label.get(0).get(0).toString())
-
-
+                        // Release
                         matImage.release()
+
+                        Toast.makeText(this,
+                                "Result:" + label[0][0].toString(),Toast.LENGTH_LONG).show()
+
                     }
 
-                    //val imageView: ImageView = findViewById(R.id.imageView)
-                    //imageView.setImageBitmap(bm)
                 }
 
             }
         }
     }
 
-    /*fun getBitmapFromUri(uri: Uri): Bitmap {
-        val parcelfileDescriptor = contentResolver.openFileDescriptor(uri, "i")
-        val fileDescriptor = parcelfileDescriptor.fileDescriptor
-        val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
-        parcelfileDescriptor.close()
-        return image
-    }
-*/
 }
