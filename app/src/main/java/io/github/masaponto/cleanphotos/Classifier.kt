7f032fa34e8e65c03c8650ff2fee1705c7aa14e5
masaponto <masaponto@gmail.com>
2018-07-29 22:37:51 +0900

    [Mod] refactor

--- app/src/main/java/io/github/masaponto/cleanphotos/Classifier.kt
+++ app/src/main/java/io/github/masaponto/cleanphotos/Classifier.kt
@@ -1,12 +1,20 @@
 package io.github.masaponto.cleanphotos
 
 import android.app.Activity
+import android.graphics.BitmapFactory
 import android.util.Log
+import android.widget.Toast
+import org.opencv.android.Utils
+import org.opencv.core.CvType
 import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
 import java.io.FileInputStream
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 import org.tensorflow.lite.Interpreter
+import java.io.File
+import java.io.FileNotFoundException
 import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -52,6 +60,38 @@ class Classifier(activity: Activity) {
         tffile.run(imageData, labelProbArray)
         imageData.clear()
         return labelProbArray
+    }
+
+    fun classifyImageFromPath(path: String): Float? {
+        val file = File(path)
+        //val file = File(dir.absolutePath + "/test.jpg")
+
+        if (file.exists()) {
+
+            val bm = BitmapFactory.decodeFile(file.path)
+
+            bm?.let {
+
+                val srcMat = Mat(bm.width, bm.height, CvType.CV_8UC3)
+                Utils.bitmapToMat(it, srcMat)
+                Imgproc.cvtColor(srcMat,srcMat,Imgproc.COLOR_BGRA2RGB)
+
+                // convert bitmap to Mat
+                val matImage = Mat(28,28, CvType.CV_8UC3)
+                Imgproc.resize(srcMat, matImage, Size(28.0, 28.0))
+                matImage.convertTo(matImage, CvType.CV_8UC3)
+
+                // classification with TF Lite
+                val label = classifyImage(matImage)
+
+                // Release
+                matImage.release()
+
+                return label[0][0]
+            }
+        }
+
+        throw Exception("Fail to load image")
     }
 
 }
--- app/src/main/java/io/github/masaponto/cleanphotos/MainActivity.kt
+++ app/src/main/java/io/github/masaponto/cleanphotos/MainActivity.kt
@@ -1,21 +1,13 @@
 package io.github.masaponto.cleanphotos
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import org.opencv.android.OpenCVLoader
-import org.opencv.android.Utils
-import org.opencv.core.CvType
 import java.io.File
-import org.opencv.core.Mat
-import org.opencv.core.Size
-import org.opencv.imgproc.Imgproc
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,39 +27,12 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             if (dir.exists()) {
+                val path = dir.absolutePath + "/IMG_20180425_122602.jpg"
+                val label: Float? = classifier.classifyImageFromPath(path)
 
-                //val file = File(dir.absolutePath + "/IMG_20180425_122554.jpg")
-                val file = File(dir.absolutePath + "/IMG_20180425_122602.jpg")
-                //val file = File(dir.absolutePath + "/test.jpg")
-
-                if (file.exists()) {
-
-                    val bm = BitmapFactory.decodeFile(file.path)
-
-                    bm?.let {
-
-                        val srcMat = Mat(bm.width, bm.height, CvType.CV_8UC3)
-                        Utils.bitmapToMat(it, srcMat)
-                        Imgproc.cvtColor(srcMat,srcMat,Imgproc.COLOR_BGRA2RGB)
-
-                        // convert bitmap to Mat
-                        val matImage = Mat(28,28, CvType.CV_8UC3)
-                        Imgproc.resize(srcMat, matImage, Size(28.0, 28.0))
-                        matImage.convertTo(matImage, CvType.CV_8UC3)
-
-                        // classification with TF Lite
-                        val label = classifier.classifyImage(matImage)
-
-                        // Release
-                        matImage.release()
-
-                        Toast.makeText(this,
-                                "Result:" + label[0][0].toString(),
-                                Toast.LENGTH_LONG).show()
-
-                    }
-                }
-
+                Toast.makeText(this,
+                        "Result:" + label.toString(),
+                        Toast.LENGTH_LONG).show()
             }
         }
     }
